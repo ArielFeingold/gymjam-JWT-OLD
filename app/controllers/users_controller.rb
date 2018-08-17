f78799beef2c082396ec859979b631506b1da806
ArielFeingold <feingold.ariel@gmail.com>
2018-08-17 19:49:54 -0400

    added pry user create errors returned

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class UsersController < ApplicationController
   before_action :authenticate_user,  only: [:index, :current, :update]
   before_action :authorize_as_admin, only: [:destroy]
@@ -11,6 +13,8 @@ def create
   user = User.new(user_params)
   if user.save
     render json: {status: 200, msg: 'User was created.'}
+  else
+    render json: {status: 400, msg: user.errors}
   end
 end
 
