d286cc595a344b8a6d5f03f64f30f1194aaa36a5
ArielFeingold <feingold.ariel@gmail.com>
2018-08-17 17:53:55 -0400

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root   'home#index'
+  get    'auth'            => 'home#auth'
+
+  post   'user_token'      => 'user_token#create'
+
+  get    '/users'          => 'users#index'
+  get    '/users/current'  => 'users#current'
+  post   '/users/create'   => 'users#create'
+  patch  '/user/:id'       => 'users#update'
+  delete '/user/:id'       => 'users#destroy'
+
 end
