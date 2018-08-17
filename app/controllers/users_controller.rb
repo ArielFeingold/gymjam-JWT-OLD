da80cc9487994d7d5c21b2a18e8a5e1659698f4d
ArielFeingold <feingold.ariel@gmail.com>
2018-08-17 17:50:34 -0400

    setup user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user,  only: [:index, :current, :update]
+  before_action :authorize_as_admin, only: [:destroy]
+  before_action :authorize,          only: [:update]
+
+  # Should work if the current_user is authenticated.
+  def index
+    render json: {status: 200, msg: 'Logged-in'}
+  end
+
+  def current
+    current_user.update!(last_login: Time.now)
+    render json: current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def authorize
+    return_unauthorized unless current_user && current_user.can_modify_user?(params[:id])
+  end
 end
