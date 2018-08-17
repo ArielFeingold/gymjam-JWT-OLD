8d24dbf0b4e14dd222d2d6a73e4da1438e2532b2
ArielFeingold <feingold.ariel@gmail.com>
2018-08-17 17:52:59 -0400

    set up user serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,31 @@ class UsersController < ApplicationController
   before_action :authorize_as_admin, only: [:destroy]
   before_action :authorize,          only: [:update]
 
-  # Should work if the current_user is authenticated.
   def index
     render json: {status: 200, msg: 'Logged-in'}
   end
+
+def create
+  user = User.new(user_params)
+  if user.save
+    render json: {status: 200, msg: 'User was created.'}
+  end
+end
+
+def update
+  user = User.find(params[:id])
+  if user.update(user_params)
+    render json: { status: 200, msg: 'User details have been updated.' }
+  end
+end
+
+# Method to delete a user, this method is only for admin accounts.
+def destroy
+  user = User.find(params[:id])
+  if user.destroy
+    render json: { status: 200, msg: 'User has been deleted.' }
+  end
+end
 
   def current
     current_user.update!(last_login: Time.now)
